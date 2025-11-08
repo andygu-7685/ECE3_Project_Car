4ba1f5bbef05f53122760c9d6ad681a6c5c09e28
Andy Gu <andygu.7685@gmail.com>
2025-11-07 17:51:00 -0800

    added led signal and abort procedure for calibration

--- ECE3_Project_Car.ino
+++ ECE3_Project_Car.ino
@@ -5,8 +5,8 @@ String dummy;
 //----------------------------------------------------------------------------------
 //Calibration
 uint16_t sensorValues[8];
-uint16_t maxVal[8] = {0};
-uint16_t minVal[8] = {0};
+uint16_t maxVal[8] = {2500,	2297,	2500,	2500,	2248,	2500,	2500,	2500};
+uint16_t minVal[8] = {619,	573,	641,	688,	619,	688,	757,	828};
 //int16_t weightVal[8] = {-8, -4, -2, -1, 1, 2, 4, 8};
 int16_t weightVal[8] = {-15, -14, -12, -8, 8, 12, 14, 15};
 
@@ -15,8 +15,8 @@ bool print_directions = true;
 
 //S1 button
 const int right_btn_pin = 73;
-unsigned long btn_press_time = 0;
-bool calibrate = false;
+const int red_led_pin = 75;
+bool calibrate = true;
 //----------------------------------------------------------------------------------
 
 
@@ -33,17 +33,22 @@ const int right_pwm_pin = 39;
 
 int left_pwm_state = 0;
 int right_pwm_state = 0;
-int base_pwm_speed = 10;
+int base_pwm_speed = 50;
 
 //true == right
 //false == left
 bool car_dir = true;
+float avg_error = 0;
 //----------------------------------------------------------------------------------
 
 //----------------------------------------------------------------------------------
 //Wheel Derivative Control
 unsigned long delta_time = 0;
 unsigned long last_delta_time = 0;
+float delta_error = 0;
+float last_error = 0;
+
+
 
 
 
@@ -63,6 +68,7 @@ void setup()
   pinMode(right_pwm_pin, OUTPUT);
 
   pinMode(right_btn_pin, INPUT_PULLUP);
+  pinMode(red_led_pin, OUTPUT);
 
   digitalWrite(left_nslp_pin,HIGH);
   digitalWrite(right_nslp_pin,HIGH);
@@ -70,15 +76,9 @@ void setup()
   digitalWrite(left_dir_pin, LOW);
   digitalWrite(right_dir_pin, LOW);
 
-  if(calibrate){
-    calibration_function();
-  }
-  else{
-    uint16_t _maxVal[8] = {2500,	2297,	2500,	2500,	2248,	2500,	2500,	2500};
-    uint16_t _minVal[8] = {619,	573,	641,	688,	619,	688,	757,	828};
-    memcpy(maxVal, _maxVal, sizeof(maxVal));
-    memcpy(minVal, _minVal, sizeof(minVal));
-  }
+  digitalWrite(red_led_pin,HIGH);
+  delay(1000);
+  digitalWrite(red_led_pin, LOW);
 
 }
 
@@ -90,22 +90,25 @@ void loop()
   //Setup
   if (print_directions)
   {
-    for (int i = 8; i > 0; i--) {
-      Serial.print("loading: ");
-      Serial.println(i);
-      delay(1000);
-    }
-    Serial.println("--------------");
-    Serial.println("This is the IR sensor calibration script");
-    Serial.println("To use, line up the car/track at the desired position (error value)");
-    Serial.println("Then, press ENTER into the serial monitor input text field.");
-    Serial.println("The leftmost column is the error; the other 8 columns are the 8 sensor values");
     print_directions = false;
+    last_delta_time = micros();
   }
 
   //wait for user input
   // while (Serial.available() == 0) {}
   // dummy = Serial.readString();
+
+  if(digitalRead(right_btn_pin) == LOW){
+    if(!calibration_function()){
+      //blink 5 times if calibration was aborted midway
+      for (unsigned char j = 0; j < 5; j++){
+        digitalWrite(red_led_pin, HIGH);
+        delay(600);
+        digitalWrite(red_led_pin, LOW);
+        delay(600);
+      }
+    }
+  }
   //-------------------------------------------------------------------------------------------------------------
 
 
@@ -114,7 +117,6 @@ void loop()
   //Sampling
   int summed_values[8] = {0};
   int weighted_sum = 0;
-  int avg_error = 0;
 
   // Take the average of 5 consecutive values for each sensor
   //the time between each value depend on excution time
@@ -141,10 +143,10 @@ void loop()
     //the last operation should always be division, because value less than 1 can be cast to 0 in int, if not handled correctly
     //the divide by 8 is for weighting purpose
     weighted_sum += (weightVal[i] * ((summed_values[i] / number_samples) - minVal[i]) * 1000) / (8 * (maxVal[i] - minVal[i]));
-    // Serial.print(weightVal[i] * ((summed_values[i] / number_samples) - minVal[i]) * 1000 /(8 * (maxVal[i] - minVal[i])));
-    // Serial.print('\t'); 
+    Serial.print(weightVal[i] * ((summed_values[i] / number_samples) - minVal[i]) * 1000 /(8 * (maxVal[i] - minVal[i])));
+    Serial.print('\t'); 
   }
-  //Serial.println();
+  Serial.println();
   //-------------------------------------------------------------------------------------------------------------
   
 
@@ -157,20 +159,32 @@ void loop()
   //true == right
   //false == left
   car_dir = (avg_error < 0);
-  int _p = abs(avg_error * 40 / 350);
-
-  left_pwm_state = base_pwm_speed;
-  right_pwm_state = base_pwm_speed;
-
-  if(car_dir){
-    left_pwm_state += _p;
-  }
-  else{
-    right_pwm_state += _p;
-  }
+  int _p = abs(avg_error * 45 / 350);
 
   delta_time = micros() - last_delta_time;
+  delta_error = avg_error - last_error;
+  float _d = abs(delta_error * 10000 / delta_time);
+
   last_delta_time = micros();
+  last_error = avg_error;
+  
+  Serial.print("_d: ");
+  Serial.print(_d);
+  Serial.print('\t');
+  Serial.print(delta_error);
+  Serial.print('\t');
+  Serial.print(delta_time/10000);
+  Serial.print('\t');
+  Serial.println();
+
+  if(car_dir){
+    left_pwm_state = base_pwm_speed + _p - _d;
+    right_pwm_state = base_pwm_speed - _p + _d;
+  }
+  else{
+    left_pwm_state = base_pwm_speed - _p + _d;
+    right_pwm_state = base_pwm_speed + _p - _d;
+  }
   //-------------------------------------------------------------------------------------------------------------
 
 
@@ -178,24 +192,44 @@ void loop()
   //-------------------------------------------------------------------------------------------------------------
   //Execution
 
-  //analogWrite(left_pwm_pin, left_pwm_state);
-  //analogWrite(right_pwm_pin, right_pwm_state);
+  analogWrite(left_pwm_pin, left_pwm_state);
+  analogWrite(right_pwm_pin, right_pwm_state);
 
 
 }
 
 
 bool calibration_function(){
-  int current_position = -40;
-  int increment_position = 4;
+  uint16_t _maxVal[8] = {0};        //temp array for new max and min
+  uint16_t _minVal[8] = {0};
   int summed_values[8] = {0};
   for (int i = 0; i < 8; i++) sensorValues[i] = 0;
+  int current_position = -40;
+  int increment_position = 4;
+  unsigned long press_start = micros();
+  unsigned long press_duration = 0;
+  digitalWrite(red_led_pin, HIGH);  //led indicator is on during calibration
 
-  for (unsigned char i = 0; i < 20; i++) {
+  for (unsigned char i = 0; i < 21; i++) {
 
     //the btn cooldown time is 500 ms
-    while ((digitalRead(right_btn_pin) == HIGH) || ((micros() - btn_press_time) < 500000)) {}
-    btn_press_time = micros();
+    while ((digitalRead(right_btn_pin) == HIGH) || ((micros() - press_start) < 500000)) {
+      //check if the button was released
+      if(press_duration != 0 && digitalRead(right_btn_pin) == HIGH){
+        press_duration = 0;
+      }
+    }
+
+    if(press_duration != 0){
+      press_duration = micros() - press_start;
+      //abort calibration when button is hold for 5 sec
+      if(press_duration > 5000000) return false;
+    }
+    else{
+      press_start = micros();
+      press_duration = 1;
+    }
+
     for (int i = 0; i < 8; i++) summed_values[i] = 0;
 
     // Take the average of 5 consecutive values for each sensor
@@ -221,8 +255,8 @@ bool calibration_function(){
     // Print average values (average value = summed_values / number_samples
     for (unsigned char j = 0; j < 8; j++) {
       int mean_value = summed_values[j] / number_samples;
-      if(maxVal[j] == 0 || mean_value > maxVal[j]) maxVal[j] = mean_value;
-      if(minVal[j] == 0 || mean_value < minVal[j]) minVal[j] = mean_value;
+      if(_maxVal[j] == 0 || mean_value > _maxVal[j]) _maxVal[j] = mean_value;
+      if(_minVal[j] == 0 || mean_value < _minVal[j]) _minVal[j] = mean_value;
       Serial.print(summed_values[j] / number_samples);
       Serial.print('\t'); 
     }
@@ -231,19 +265,31 @@ bool calibration_function(){
 
   Serial.print("Max: "); 
   for (unsigned char i = 0; i < 8; i++) {
-    Serial.print(maxVal[i]);
+    Serial.print(_maxVal[i]);
     Serial.print('\t'); 
   }
   Serial.println();
 
   Serial.print("Min: "); 
   for (unsigned char i = 0; i < 8; i++) {
-    Serial.print(minVal[i]);
+    Serial.print(_minVal[i]);
     Serial.print('\t'); 
   }
   Serial.println();
 
+  //abort calibration when button is hold for 5 sec
+  while (press_duration != 0 && digitalRead(right_btn_pin) == LOW){
+    press_duration = micros() - press_start;
+    if(press_duration > 5000000) return false;
+  }
+
+  //copy the new calibration max and min to maxVal and minVal
+  memcpy(maxVal, _maxVal, sizeof(maxVal));
+  memcpy(minVal, _minVal, sizeof(minVal));
+  //clean sensor value array
   for (int i = 0; i < 8; i++) sensorValues[i] = 0;
+  digitalWrite(red_led_pin, LOW);
+  return true;
 }
 
 
