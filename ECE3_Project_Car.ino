4f98f4a669c2f5db19c4092debedd6cf2224aaa1
Andy Gu <andygu.7685@gmail.com>
2025-11-14 11:17:14 -0800

    add stop function

--- ECE3_Project_Car.ino
+++ ECE3_Project_Car.ino
@@ -8,12 +8,14 @@ uint16_t sensorValues[8];
 //Lab
 // uint16_t maxVal[8] = {2500,	2297,	2500,	2500,	2248,	2500,	2500,	2500};
 // uint16_t minVal[8] = {619,	573,	641,	688,	619,	688,	757,	828};
+uint16_t maxVal[8] = {2321,	1835,	1891,	2083,	1770,	1839,	2041,	2329};
+uint16_t minVal[8] = {721,	630,	723,	747,	653,	698,	749,	821};
 //Apartment
 // uint16_t maxVal[8] = {2478,	2318,	2219,	2148,	1952,	2338,	2338,	2500};
 // uint16_t minVal[8] = {811,	626,	723,	751,	605,	668,	791,	817};
 //PCC
-uint16_t maxVal[8] = {2059, 1935, 2013, 2050, 1914, 1871, 1986, 2391};
-uint16_t minVal[8] = {750, 683, 723, 775, 694, 702, 771,	823};
+//uint16_t maxVal[8] = {2059, 1935, 2013, 2050, 1914, 1871, 1986, 2391};
+//uint16_t minVal[8] = {750, 683, 723, 775, 694, 702, 771,	823};
 
 //int16_t weightVal[8] = {-8, -4, -2, -1, 1, 2, 4, 8};
 int16_t weightVal[8] = {-15, -14, -12, -8, 8, 12, 14, 15};
@@ -41,7 +43,7 @@ const int right_pwm_pin = 39;
 
 int left_pwm_state = 0;
 int right_pwm_state = 0;
-int base_pwm_speed = 50;
+int base_pwm_speed = 20;
 
 //true == right
 //false == left
@@ -55,6 +57,8 @@ unsigned long delta_time = 0;
 unsigned long last_delta_time = 0;
 float delta_error = 0;
 float last_error = 0;
+
+int obstacle_ctr = 0;
 
 
 
@@ -124,6 +128,7 @@ void loop()
   //-------------------------------------------------------------------------------------------------------------
   //Sampling
   int summed_values[8] = {0};
+  int non_weighted_sum = 0;
   int weighted_sum = 0;
 
   // Take the average of 5 consecutive values for each sensor
@@ -151,6 +156,7 @@ void loop()
     //the last operation should always be division, because value less than 1 can be cast to 0 in int, if not handled correctly
     //the divide by 8 is for weighting purpose
     weighted_sum += (weightVal[i] * ((summed_values[i] / number_samples) - minVal[i]) * 1000) / (8 * (maxVal[i] - minVal[i]));
+    non_weighted_sum += ((summed_values[i] / number_samples) - minVal[i]) * 1000 / (maxVal[i] - minVal[i]);
     // Serial.print(weightVal[i] * ((summed_values[i] / number_samples) - minVal[i]) * 1000 /(8 * (maxVal[i] - minVal[i])));
     // Serial.print('\t'); 
   }
@@ -162,16 +168,30 @@ void loop()
   //-------------------------------------------------------------------------------------------------------------
   //Calculation
   //take average error of the 8 sensors
+  if(non_weighted_sum >= 8 * 700){
+    // if(!calibration_function()){
+    //   //blink 5 times if calibration was aborted midway
+    //   for (unsigned char j = 0; j < 5; j++){
+    //     digitalWrite(red_led_pin, HIGH);
+    //     delay(600);
+    //     digitalWrite(red_led_pin, LOW);
+    //     delay(600);
+    //   }
+    // }
+
+  }
+
+
   avg_error = weighted_sum / 8 ;
 
   //true == right
   //false == left
   car_dir = (avg_error < 0);
-  int _p = abs(avg_error * 60 / 350);
+  int _p = abs(avg_error * 20 / 350);
 
   delta_time = micros() - last_delta_time;
   delta_error = avg_error - last_error;
-  float _d = abs(delta_error * 8000 / delta_time);
+  float _d = abs(delta_error * 4000 / delta_time);
 
   last_delta_time = micros();
   last_error = avg_error;
@@ -205,6 +225,30 @@ void loop()
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 bool calibration_function(){
@@ -307,5 +351,11 @@ bool calibration_function(){
   digitalWrite(red_led_pin, LOW);
   return true;
 }
+
+
+
+
+
+
 
 
