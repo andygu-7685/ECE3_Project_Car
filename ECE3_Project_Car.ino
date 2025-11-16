aba0f4288252a81c8956334f66d5f38e705d3b59
Andy Gu <andygu.7685@gmail.com>
2025-11-15 18:54:26 -0800

    improved calibration function with max avg error, and cross piece detection

--- ECE3_Project_Car.ino
+++ ECE3_Project_Car.ino
@@ -8,11 +8,12 @@ uint16_t sensorValues[8];
 //Lab
 // uint16_t maxVal[8] = {2500,	2297,	2500,	2500,	2248,	2500,	2500,	2500};
 // uint16_t minVal[8] = {619,	573,	641,	688,	619,	688,	757,	828};
-uint16_t maxVal[8] = {2321,	1835,	1891,	2083,	1770,	1839,	2041,	2329};
-uint16_t minVal[8] = {721,	630,	723,	747,	653,	698,	749,	821};
+// uint16_t maxVal[8] = {2321,	1835,	1891,	2083,	1770,	1839,	2041,	2329};
+// uint16_t minVal[8] = {721,	630,	723,	747,	653,	698,	749,	821};
 //Apartment
-// uint16_t maxVal[8] = {2478,	2318,	2219,	2148,	1952,	2338,	2338,	2500};
-// uint16_t minVal[8] = {811,	626,	723,	751,	605,	668,	791,	817};
+uint16_t maxVal[8] = {2478,	2318,	2219,	2148,	1952,	2338,	2338,	2500};
+uint16_t minVal[8] = {811,	626,	723,	751,	605,	668,	791,	817};
+int16_t max_avg_error = 248;
 //PCC
 //uint16_t maxVal[8] = {2059, 1935, 2013, 2050, 1914, 1871, 1986, 2391};
 //uint16_t minVal[8] = {750, 683, 723, 775, 694, 702, 771,	823};
@@ -43,12 +44,14 @@ const int right_pwm_pin = 39;
 
 int left_pwm_state = 0;
 int right_pwm_state = 0;
-int base_pwm_speed = 20;
+int base_pwm_speed = 30;
 
 //true == right
 //false == left
 bool car_dir = true;
 float avg_error = 0;
+float K_p = 0.9 * base_pwm_speed / max_avg_error;
+float K_d = 6.3 * K_p;
 //----------------------------------------------------------------------------------
 
 //----------------------------------------------------------------------------------
@@ -58,12 +61,13 @@ unsigned long last_delta_time = 0;
 float delta_error = 0;
 float last_error = 0;
 
-int obstacle_ctr = 0;
 
 
-bool bar(){
-  return non_weighted_sum >= 8 * 700;
-}
+
+uint16_t obstacle_ctr = 0;
+uint16_t crosspiece = 0;
+
+
 
 
 
@@ -129,13 +133,13 @@ void loop()
 
   //-------------------------------------------------------------------------------------------------------------
   //Sampling
-  int summed_values[8] = {0};
-  int non_weighted_sum = 0;
-  int weighted_sum = 0;
+  uint16_t summed_values[8] = {0};
+  uint16_t non_weighted_sum = 0;
+  int16_t weighted_sum = 0;
 
   // Take the average of 5 consecutive values for each sensor
   //the time between each value depend on excution time
-  for (int j = 0; j < number_samples; j++){
+  for (unsigned char j = 0; j < number_samples; j++){
     // Read all 8 raw sensor values into array
     ECE3_read_IR(sensorValues);
 
@@ -169,8 +173,9 @@ void loop()
 
   //-------------------------------------------------------------------------------------------------------------
   //Calculation
-  //take average error of the 8 sensors
-  if(non_weighted_sum >= 8 * 750){
+
+  //detect black signal band
+  if(non_weighted_sum >= 8 * 800 && non_weighted_sum <= 8 * 2400){
     if(!calibration_function()){
       //blink 5 times if calibration was aborted midway
       for (unsigned char j = 0; j < 5; j++){
@@ -180,20 +185,30 @@ void loop()
         delay(600);
       }
     }
-
   }
+  
+  //detect crosspiece period
+  if(non_weighted_sum == 8 * 2500)
+    (crosspiece == 2) ? crosspiece = 0 : crosspiece = 2;
+  else
+    (crosspiece == 2) ? crosspiece = 1 : crosspiece = 0;
 
-
+  //take average error of the 8 sensors
   avg_error = weighted_sum / 8 ;
 
   //true == right
   //false == left
   car_dir = (avg_error < 0);
-  int _p = abs(avg_error * 20 / 350);
+  int16_t _p = abs(avg_error * K_p);
 
   delta_time = micros() - last_delta_time;
   delta_error = avg_error - last_error;
-  float _d = abs(delta_error * 4000 / delta_time);
+  //delta time is around 5ms, delta error is around 25 to 50
+  //multiply by 1000 covert to delta time to ms
+  //multiply by 10 for scaling factor
+  float _d = abs(delta_error * 10 * 1000 * K_d / delta_time);
+  //set _d to zero if current or prev error is a cross piece
+  if(crosspiece != 0) _d = 0; 
 
   last_delta_time = micros();
   last_error = avg_error;
@@ -256,12 +271,16 @@ void loop()
 bool calibration_function(){
   uint16_t _maxVal[8] = {0};        //temp array for new max and min
   uint16_t _minVal[8] = {0};
-  int summed_values[8] = {0};
-  for (int i = 0; i < 8; i++) sensorValues[i] = 0;
-  int current_position = -40;
-  int increment_position = 4;
+  uint16_t summed_values[8] = {0};
+  for (unsigned char i = 0; i < 8; i++) sensorValues[i] = 0;
+  int16_t current_position = -40;
+  int16_t increment_position = 4;
   unsigned long press_start = micros();
   unsigned long press_duration = 0;
+  uint16_t mean_matrix[21][8] = {0};
+  int16_t _max_avg_error = 0;
+  int16_t current_error = 0;
+
   digitalWrite(red_led_pin, HIGH);  //led indicator is on during calibration
   analogWrite(left_pwm_pin, 0);
   analogWrite(right_pwm_pin, 0);
@@ -291,11 +310,11 @@ bool calibration_function(){
       press_duration = 1;
     }
 
-    for (int i = 0; i < 8; i++) summed_values[i] = 0;
+    for (unsigned char j = 0; j < 8; j++) summed_values[j] = 0;
 
     // Take the average of 5 consecutive values for each sensor
     //the time between each value depend on excution time
-    for (int j = 0; j < number_samples; j++){
+    for (unsigned char j = 0; j < number_samples; j++){
       // Read all 8 raw sensor values into array
       ECE3_read_IR(sensorValues);
 
@@ -316,13 +335,21 @@ bool calibration_function(){
     // Print average values (average value = summed_values / number_samples
     for (unsigned char j = 0; j < 8; j++) {
       int mean_value = summed_values[j] / number_samples;
+      mean_matrix[i][j] = mean_value;
       if(_maxVal[j] == 0 || mean_value > _maxVal[j]) _maxVal[j] = mean_value;
       if(_minVal[j] == 0 || mean_value < _minVal[j]) _minVal[j] = mean_value;
-      //weighted_sum += (weightVal[i] * ((summed_values[j] / number_samples) - minVal[i]) * 1000) / (8 * (maxVal[i] - minVal[i]));
-      Serial.print(summed_values[j] / number_samples);
-      Serial.print('\t'); 
+      Serial.print(mean_value);
+      Serial.print('\t');
     }
     Serial.println();
+  }
+
+  //Max avg error calculation
+  for (unsigned char i = 0; i < 21; i++) {
+    current_error = 0;
+    for (unsigned char j = 0; j < 8; j++) 
+      current_error += (weightVal[j] * (mean_matrix[i][j] - _minVal[j]) * 1000) / (8 * 8 * (_maxVal[j] - _minVal[j]));
+    if(_max_avg_error <= current_error) _max_avg_error = current_error;
   }
 
   Serial.print("Max: "); 
@@ -339,6 +366,10 @@ bool calibration_function(){
   }
   Serial.println();
 
+  Serial.print("Max Error: "); 
+  Serial.print(_max_avg_error);
+  Serial.println();
+
   //abort calibration when button is hold for 5 sec
   while (press_duration != 0 && digitalRead(right_btn_pin) == LOW){
     press_duration = micros() - press_start;
@@ -346,8 +377,12 @@ bool calibration_function(){
   }
 
   //copy the new calibration max and min to maxVal and minVal
+  //copy the new max avg error
   memcpy(maxVal, _maxVal, sizeof(maxVal));
   memcpy(minVal, _minVal, sizeof(minVal));
+  max_avg_error = _max_avg_error;
+  K_p = 0.9 * base_pwm_speed / max_avg_error;
+  K_d = 6.3 * K_p;
   //clean sensor value array
   for (int i = 0; i < 8; i++) sensorValues[i] = 0;
   digitalWrite(red_led_pin, LOW);
